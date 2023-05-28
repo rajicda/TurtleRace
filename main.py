df087a089299d1129032a45022c488007a545a52
rajicda <rajic.danijel11@gmail.com>
2023-05-28 11:05:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=500, height=400)
+colors = ["red", "green", "blue", "orange", "yellow", "purple"]
+user_bet = screen.textinput(title="Make your bet.", prompt=f"Which turtle will win the race? Enter a color: \n"
+                                                           f" {colors}")
+all_turtles = []
+is_race_on = False
+y = 90
+for each_turtle_color in colors:
+    turtle = Turtle(shape="turtle")
+    turtle.penup()
+    turtle.speed(1)
+    turtle.color(each_turtle_color)
+    turtle.goto(x=-240, y=y)
+    y -= 30
+    all_turtles.append(turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You have won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You have lost! The {winning_color} turtle is the winner!")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+screen.exitonclick()
